0c26c76e7e088854d94ab6888c57835993a4aeaa
Adrian Lawson <adrianglawson@gmail.com>
2022-09-02 11:09:05 -0700

    cleaned up code

--- claim.js
+++ claim.js
@@ -30,8 +30,6 @@ exports.helloWorld = async (req, res) => {
     }
   ];
 
-  // A Web3Provider wraps a standard Web3 provider, which is
-  // what MetaMask injects as window.ethereum into each page
   const provider = new ethers.providers.JsonRpcProvider(rpcUrl);
 
   const wallet = new ethers.Wallet(privateKey, provider);
